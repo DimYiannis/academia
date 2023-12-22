72961a0198355dc1ac03cfdba00add739bc5b677
yiannis <107484245+DimYiannis@users.noreply.github.com>
2023-12-22 15:14:43 +0200

    adding loading state for edit

--- client/src/components/edit.vue
+++ client/src/components/edit.vue
@@ -4,7 +4,7 @@
     @click.self="closemodal"
   >
     <div
-      class="w-full h-full mobile:w-[500px] mobile:h-[550px] relative mobile:mt-10 rounded-lg bg-white z-50"
+      class="w-full h-full mobile:w-[500px] mobile:h-[550px] relative mobile:mt-10 rounded-lg bg-white z-40"
     >
       <div class="h-full m-5">
         <div class="flex h-fit">
@@ -147,6 +147,45 @@
         </div>
       </div>
     </div>
+    <!-- upload state -->
+    <div
+      v-show="loading || uploadMessage"
+      class="z-50 fixed mobile:top-10 top-14 left-1/2 text-white"
+    >
+      <div
+        id="loader"
+        v-show="loading"
+        class="relative transform -translate-x-1/2 -translate-y-1/2
+         bg-[#388aef] p-4 rounded shadow"
+      >
+        <svg
+          aria-hidden="true"
+          role="status"
+          class="inline w-4 h-4 me-3 text-white animate-spin"
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="#E5E7EB"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentColor"
+          />
+        </svg>
+        Uploading...
+      </div>
+      <div
+        id="message"
+        v-show="uploadMessage"
+        class="relative transform -translate-x-1/2 -translate-y-1/2 
+        bg-[#388aef] p-4 rounded shadow"
+      >
+        {{ uploadMessage }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -159,6 +198,8 @@ export default {
       userInfo: "",
       backgroundPic: "",
       profilePic: "",
+      loading: false,
+      uploadMessage: "",
     };
   },
   props: {
@@ -214,48 +255,76 @@ export default {
 
     async uploadBackgroundImg() {
       try {
-        console.log(this.backgroundPic); 
+        console.log(this.backgroundPic);
+        this.loading = true;
 
-        axios.post(
+        const response = await axios.post(
           "http://localhost:5000/api/v1/users/uploadbackimage",
           { image: this.backgroundPic },
           {
             withCredentials: true,
             headers: {
-              "Content-Type": "multipart/form-data", 
+              "Content-Type": "multipart/form-data",
             },
           }
         );
+        //message
+        if (response && response.status === 200) {
+          this.uploadMessage = "Background image uploaded successfully!";
+        } else {
+          this.uploadMessage = "Failed to upload background image.";
+        }
       } catch (error) {
-        console.error("Error uploading file:", error);
+        console.error("Error uploading file:", error.response.data);
+        this.uploadMessage = error.response.data.error;
+        
+      } finally {
+        this.loading = false;
       }
+      setTimeout(() => {
+        this.uploadMessage = "";
+      }, 3000);
     },
 
     async uploadProfileImg() {
       try {
-        console.log(this.profilePic); 
+        this.loading = true;
+        console.log(this.profilePic);
 
-        axios.post(
+        const response = await axios.post(
           "http://localhost:5000/api/v1/users/uploadprofimage",
           { image: this.profilePic },
           {
             withCredentials: true,
             headers: {
-              "Content-Type": "multipart/form-data", 
+              "Content-Type": "multipart/form-data",
             },
           }
         );
+
+        // message
+        if (response.status === 200) {
+          this.uploadMessage = "Profile image uploaded successfully!";
+        } else {
+          this.uploadMessage = "Failed to upload profile image.";
+        }
       } catch (error) {
         console.error("Error uploading file:", error);
+        this.uploadMessage = error.response.data.error
+      } finally {
+        this.loading = false;
       }
+      setTimeout(() => {
+        this.uploadMessage = "";
+      }, 3000);
     },
 
-
     update() {
-      //this.updatedetails();
-      //this.uploadBackgroundImg();
-      this.uploadProfileImg()
+      this.updatedetails();
+      this.uploadBackgroundImg();
+      this.uploadProfileImg();
       console.log(this.userr);
+     
     },
   },
 };
--- client/src/pages/home.vue
+++ client/src/pages/home.vue
@@ -6,7 +6,7 @@
       <div class=" w-1/2 mt-32 pl-5 ">
         <h1 class="text-3xl text-[#388aef] font-bold">Academia</h1>
         <p class="my-5 text-start text-lg">
-          Where scientific papers meet shared thoughts. 
+          Where scientific research meet shared thoughts. 
           Explore, share, and discuss the latest research with a global community of 
           like-minded individuals. Join the conversation and fuel your intellectual 
           journey in a space designed for collaboration and discovery. 
